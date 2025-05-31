8a8ed6198e61bc9cc726e5f90bacb979ac1cbde8
SamayRayj <rayushjain8099@gmail.com>
2025-05-31 11:36:31 +0530

    Added Email and Phone no. Regex in InputValidator of utility package

--- src/main/java/com/abes/foodDeliveryApplication/utils/InputValidator.java
+++ src/main/java/com/abes/foodDeliveryApplication/utils/InputValidator.java
@@ -1,5 +1,9 @@
 package com.abes.foodDeliveryApplication.utils;
 
-public class InputValidator {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class InputValidator {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+    private static final Pattern PHONE_PATTERN = Pattern.compile("^\\d{10}$");
 }
