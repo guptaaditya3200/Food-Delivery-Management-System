0734e8ac0ced9796670925c4589232f3618694d9
SamayRayj <rayushjain8099@gmail.com>
2025-05-31 11:09:15 +0530

    collectionUtil update

--- src/main/java/com/abes/foodDeliveryApplication/utils/CollectionUtil.java
+++ src/main/java/com/abes/foodDeliveryApplication/utils/CollectionUtil.java
@@ -1,5 +1,25 @@
 package com.abes.foodDeliveryApplication.utils;
 
+import com.abes.foodDeliveryApplication.dto.User;
+import com.abes.foodDeliveryApplication.dto.FoodItem;
+import com.abes.foodDeliveryApplication.dto.Order;
+import java.util.*;
+
 public class CollectionUtil {
+    // Storage for all entities
+    private static final Map<String, User> users = new HashMap<>();
+    private static final Map<String, User> customers = new HashMap<>();
+    private static final Map<String, User> deliveryPersons = new HashMap<>();
+    private static final Map<String, User> managers = new HashMap<>();
+    private static final Map<FoodItem, Integer> inventory = new HashMap<>();
+    private static final Map<String, Order> orders = new HashMap<>();
+
+    // Static initialization with default manager
+    static {
+        User defaultManager = new User("MGR001", "System Manager", "manager@system.com",
+                "9999999999", "admin123", "MANAGER");
+        managers.put(defaultManager.getId(), defaultManager);
+        users.put(defaultManager.getId(), defaultManager);
+    }
 
 }
