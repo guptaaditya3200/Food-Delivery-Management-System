63308d6ef5a1e2ed32f8e712437629f773c51e39
Shayy841 <namangoel841@gmail.com>
2025-05-31 14:30:01 +0530

    User service and user service implementation add

--- src/main/java/com/abes/foodDeliveryApplication/services/UserService.java
+++ src/main/java/com/abes/foodDeliveryApplication/services/UserService.java
@@ -1,5 +1,24 @@
 package com.abes.foodDeliveryApplication.services;
 
-public class UserService {
+import com.abes.foodDeliveryApplication.dto.User;
+import com.abes.foodDeliveryApplication.exception.UserNotFoundException;
+import com.abes.foodDeliveryApplication.exception.InvalidCredentialsException;
+import java.util.List;
 
+public interface UserService {
+    void registerCustomer(String id, String name, String email, String phone, String password);
+
+    void registerDeliveryPerson(String id, String name, String email, String phone, String password);
+
+    User authenticateUser(String id, String password) throws UserNotFoundException, InvalidCredentialsException;
+
+    User getUserById(String id) throws UserNotFoundException;
+
+    List<User> getAllDeliveryPersons();
+
+    List<User> getAvailableDeliveryPersons();
+
+    void updateUserAvailability(String userId, boolean available) throws UserNotFoundException;
+
+    void removeDeliveryPerson(String id) throws UserNotFoundException;
 }
